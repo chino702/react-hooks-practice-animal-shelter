32b7be620cf8668f9b6838136b3bc4eb5b072157
chino702 <chinox3702@gmail.com>
2023-08-09 21:15:19 -0700

    finished the animal shelter practice for react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (newType) => {
+    setFilters({ type: newType });
+  };
+
+  const onFindPetsClick = () => {
+    const url = filters.type === "all" ? "http://localhost:3001/pets" : `http://localhost:3001/pets?type=${filters.type}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPets(data);
+      });
+  };
+
+  const onAdoptPet = (petId) => {
+    const updatedPets = pets.map((pet) =>
+      pet.id === petId ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +35,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={onChangeType}
+              onFindPetsClick={onFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
